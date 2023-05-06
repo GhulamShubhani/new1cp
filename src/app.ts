8989dcee6f3497dcf32739197bc79f7545c4eb34
GHULAM SHUBHANI <ghulam@payfinix.in>
2023-05-06 23:54:17 +0530

    3rdd commit

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import { config } from 'dotenv'
-config();
+// import { config } from 'dotenv'
+// config();
 
 
 import adminRouter from "./routes/Admin";
@@ -14,13 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8000;
-const server: string = process.env.CONNECT_DATABASE!
+// const server: string = process.env.CONNECT_DATABASE!
 
 app.use(adminRouter);
 app.use(LandLordRouter);
 app.use(PropertyRouter);
 
-mongoose.connect(server)
+mongoose.connect("mongodb+srv://admin:admin@shubhani1.a80ahjo.mongodb.net/Shubhani-DataBase?retryWrites=true&w=majority")
   .then(() => {
     app.listen(PORT, () => {
       console.log(`server is started at ${PORT}`);
